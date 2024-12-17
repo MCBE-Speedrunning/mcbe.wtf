7d726b53ccabadda43f3c31e4336d8e9e843364a
Luca Matei Pintilie <luca@lucamatei.com>
2024-12-17 10:27:56 +0100

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! theme: use designsystemet

--- prep.sh
+++ prep.sh
@@ -3,16 +3,15 @@
 pnpm install --frozen-lockfile
 rm -r "themes/minecraft/assets/designsystemet-css" "themes/minecraft/assets/designsystemet-minecraft" "themes/minecraft/design-tokens"
 cp -r "node_modules/@digdir/designsystemet-css/dist/src" "themes/minecraft/assets/designsystemet-css"
-cd "themes/minecraft"
 
-pnpx @digdir/designsystemet@next tokens create \
+pnpm exec designsystemet tokens create \
 	--main-colors "accent:#aadb74" \
 	--neutral-color "#0d0d0d" \
 	--support-colors "brand1:#bb5f61" "brand2:#68dfd5" "brand3:#6a431f" \
 	--theme "minecraft" \
-	--write "./design-tokens"
+	--write "./themes/minecraft/design-tokens"
 
-pnpx @digdir/designsystemet@next tokens build \
-	--tokens "./design-tokens" \
-	--out "./assets/designsystemet-minecraft"
+pnpm exec designsystemet tokens build \
+	--tokens "./themes/minecraft/design-tokens" \
+	--out "./themes/minecraft/assets/designsystemet-minecraft"
 
